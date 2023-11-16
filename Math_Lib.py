7d4a0a4c894dc43972de97e70d460dcf92acde79
Mobin Mousavi <87776415+MobinMousavi@users.noreply.github.com>
2023-11-16 15:38:28 +0330

    Adding Math Library

---
+++ Math_Lib.py
@@ -0,0 +1,36 @@
+import math
+
+print(math.ceil(3.6))
+print(math.floor(3.6))
+
+print(math.comb(10,5))
+
+print(math.factorial(5))
+
+print(math.fabs(-6))
+
+L = [1,2,3,4,5]
+print(math.fsum(L))
+
+print(math.gcd(3,6))
+print(math.lcm(3,6))
+
+print(math.perm(5,2))
+
+numbers = list(range(10,21))
+print(math.prod(numbers))
+
+print(math.log2(4))
+print(math.log10(1000))
+
+x = math.pow(3,2)
+print(x)
+
+print(math.sqrt(100))
+
+a = math.radians(60)
+print(math.cos(a))
+print(math.sin(a))
+print(math.tan(a))
+
+print(math.pi)
